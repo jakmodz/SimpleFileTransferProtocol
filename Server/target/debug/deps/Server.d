2cf3c3a59021b60627a7a721f17773d29cacfd07
jakmodz <144469844+jakmodz@users.noreply.github.com>
2024-11-09 23:25:31 +0100

    Delete Server/target directory

--- Server/target/debug/deps/Server.d
+++
@@ -1,6 +0,0 @@
-C:\Users\123ja\Desktop\FIleTransferProtocol\Server\target\debug\deps\Server.exe: src/main.rs src\tcp_server.rs
-
-C:\Users\123ja\Desktop\FIleTransferProtocol\Server\target\debug\deps\Server.d: src/main.rs src\tcp_server.rs
-
-src/main.rs:
-src\tcp_server.rs:
