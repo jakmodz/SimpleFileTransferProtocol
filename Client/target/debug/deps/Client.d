750e13ddfbda8fb5af13da0cfb9bef5f24adf579
jakmodz <144469844+jakmodz@users.noreply.github.com>
2024-11-09 23:26:09 +0100

    Delete Client/target directory

--- Client/target/debug/deps/Client.d
+++
@@ -1,7 +0,0 @@
-C:\Users\123ja\Desktop\FIleTransferProtocol\Client\target\debug\deps\Client.exe: src/main.rs src\cli.rs src\client.rs
-
-C:\Users\123ja\Desktop\FIleTransferProtocol\Client\target\debug\deps\Client.d: src/main.rs src\cli.rs src\client.rs
-
-src/main.rs:
-src\cli.rs:
-src\client.rs:
